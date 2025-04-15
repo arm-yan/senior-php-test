6f21ae55609ce87f07968e90868c3059ee90eb4e
Arman <akarapetyan@code-pilots.ru>
2025-04-15 14:27:15 +0400

    Rewrite index.php

--- php-order-service/public/index.php
+++ php-order-service/public/index.php
@@ -1,18 +1,26 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once DIR . '/../vendor/autoload.php';
 
 use App\Controller\OrderController;
 
 $controller = new OrderController();
-
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($uri === '/orders' && $method === 'POST') {
-    $controller->createOrder();
-} elseif ($uri === '/orders' && $method === 'GET') {
-    $controller->getOrders();
+header('Content-Type: application/json');
+
+$routes = [
+    'POST' => [
+        '/orders' => [$controller, 'createOrder'],
+    ],
+    'GET' => [
+        '/orders' => [$controller, 'getOrders'],
+    ],
+];
+
+if (isset($routes[$method][$uri])) {
+    call_user_func($routes[$method][$uri]);
 } else {
     http_response_code(404);
     echo json_encode(['message' => 'Not Found']);
